2a51525f5994ef2a3c6be8f70b1b185a175e9c2e
4Rjee <slavasleychezB4trider>
2021-02-02 10:20:03 +0300

    added code

--- demo/main.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2020 Kochurin Nikita kochurin.nikita@gmail.com
-#include <NonCopyableTypeStack.hpp>
-
-int main() {
-
-}
--- sources/Stack.cpp
+++
@@ -1,2 +0,0 @@
-// Copyright 2020 Kochurin Nikita kochurin.nikita@gmail.com
-#include "Stack.hpp"
